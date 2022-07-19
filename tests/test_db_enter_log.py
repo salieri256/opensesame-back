8b7be92e8d914d6c80dd5b1fed37aa4de5c88d40
さりえり <nakakou201250@gmail.com>
2022-07-19 17:38:39 +0900

    Implement test code for DB operations

---
+++ tests/test_db_enter_log.py
@@ -0,0 +1,149 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../src/'))
+
+import sqlite3
+from db import EnterLogDb
+
+def test_db_enter_log_init():
+    dbPath = 'test_db_enter_log_init.db'
+
+    if os.path.isfile(dbPath):
+        os.remove(dbPath)
+
+    EnterLogDb(dbPath)
+
+    con = sqlite3.connect(dbPath)
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+
+    enterLogTableNum = cur.execute('SELECT COUNT(*) FROM sqlite_master WHERE TYPE="table" AND name="ENTERLOG"').fetchone()[0]
+
+    assert enterLogTableNum == 1
+
+def test_db_enter_log_add():
+    dbPath = 'test_db_enter_log_add.db'
+
+    if os.path.isfile(dbPath):
+        os.remove(dbPath)
+    
+    enterDb = EnterLogDb(dbPath)
+
+    entered = 0
+    unixTime = 0
+    userId = 0
+    enterLog = enterDb.add(entered, unixTime, userId)
+    if enterLog == None:
+        assert False
+
+    assert enterLog['entered'] == entered
+    assert enterLog['unixTime'] == unixTime
+    assert enterLog['userId'] == userId
+
+def test_db_enter_log_get():
+    dbPath = 'test_db_enter_log_get.db'
+
+    if os.path.isfile(dbPath):
+        os.remove(dbPath)
+    
+    enterDb = EnterLogDb(dbPath)
+
+    enter1 = enterDb.add(0, 0, 0)
+    if enter1 == None:
+        assert False
+    
+    enter2 = enterDb.get( enter1['id'] )
+    if enter2 == None:
+        assert False
+    
+    assert enter1['id'] == enter2['id']
+    assert enter1['entered'] == enter2['entered']
+    assert enter1['unixTime'] == enter2['unixTime']
+    assert enter1['userId'] == enter2['userId']
+
+def test_db_enter_log_get_not_exist():
+    dbPath = 'test_db_enter_log_get_not_exist.db'
+
+    if os.path.isfile(dbPath):
+        os.remove(dbPath)
+    
+    enterDb = EnterLogDb(dbPath)
+
+    enter = enterDb.get(0)
+    assert enter == None
+
+def test_db_enter_log_getAll():
+    dbPath = 'test_db_enter_log_getAll.db'
+
+    if os.path.isfile(dbPath):
+        os.remove(dbPath)
+    
+    enterDb = EnterLogDb(dbPath)
+
+    enterDb.add(0, 0, 0)
+    enterDb.add(0, 0, 0)
+
+    enterList = enterDb.getAll()
+    assert len(enterList) == 2
+
+def test_db_enter_log_set():
+    dbPath = 'test_db_enter_log_set.db'
+
+    if os.path.isfile(dbPath):
+        os.remove(dbPath)
+    
+    enterDb = EnterLogDb(dbPath)
+
+    enter1 = enterDb.add(0, 0, 0)
+    if enter1 == None:
+        assert False
+
+    enter2 = enterDb.set( enter1['id'], 1, 1000, 1 )
+    if enter2 == None:
+        assert False
+    
+    assert enter1['id'] == enter2['id']
+    assert enter2['entered'] == 1
+    assert enter2['unixTime'] == 1000
+    assert enter2['userId'] == 1
+
+def test_db_enter_log_set_not_exist():
+    dbPath = 'test_db_enter_log_set_not_exist.db'
+
+    if os.path.isfile(dbPath):
+        os.remove(dbPath)
+    
+    enterDb = EnterLogDb(dbPath)
+
+    enter = enterDb.set(0, 0, 0, 0)
+    assert enter == None
+
+def test_db_enter_log_delete():
+    dbPath = 'test_db_enter_log_delete.db'
+
+    if os.path.isfile(dbPath):
+        os.remove(dbPath)
+    
+    enterDb = EnterLogDb(dbPath)
+
+    enter1 = enterDb.add(0, 0, 0)
+    if enter1 == None:
+        assert False
+
+    enterDb.delete(enter1['id'])
+
+    enter2 = enterDb.get(enter1['id'])
+    assert enter2 == None
+
+def test_db_enter_log_delete_not_exist():
+    dbPath = 'test_db_enter_log_delete_not_exist.db'
+
+    if os.path.isfile(dbPath):
+        os.remove(dbPath)
+    
+    enterDb = EnterLogDb(dbPath)
+
+    enterDb.delete(0)
+
+    enter = enterDb.get(0)
+    assert enter == None
---
+++ tests/test_db_nfc.py
@@ -0,0 +1,145 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../src/'))
+
+import sqlite3
+from db import NfcDb
+
+def test_db_nfc_init():
+    dbPath = 'test_db_nfc_init.db'
+
+    if os.path.isfile(dbPath):
+        os.remove(dbPath)
+
+    NfcDb(dbPath)
+
+    con = sqlite3.connect(dbPath)
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+
+    nfcTableNum = cur.execute('SELECT COUNT(*) FROM sqlite_master WHERE TYPE="table" AND name="NFC"').fetchone()[0]
+
+    assert nfcTableNum == 1
+
+def test_db_nfc_add():
+    dbPath = 'test_db_nfc_add.db'
+
+    if os.path.isfile(dbPath):
+        os.remove(dbPath)
+    
+    nfcDb = NfcDb(dbPath)
+
+    nfcId = ''
+    userId = 0
+    nfc = nfcDb.add(nfcId, userId)
+    if nfc == None:
+        assert False
+
+    assert nfc['nfcId'] == nfcId
+    assert nfc['nfcId'] == nfcId
+
+def test_db_nfc_get():
+    dbPath = 'test_db_nfc_get.db'
+
+    if os.path.isfile(dbPath):
+        os.remove(dbPath)
+    
+    nfcDb = NfcDb(dbPath)
+
+    nfc1 = nfcDb.add('', 0)
+    if nfc1 == None:
+        assert False
+    
+    nfc2 = nfcDb.get( nfc1['id'] )
+    if nfc2 == None:
+        assert False
+    
+    assert nfc1['id'] == nfc2['id']
+    assert nfc1['nfcId'] == nfc2['nfcId']
+    assert nfc1['userId'] == nfc2['userId']
+
+def test_db_nfc_get_not_exist():
+    dbPath = 'test_db_nfc_get_not_exist.db'
+
+    if os.path.isfile(dbPath):
+        os.remove(dbPath)
+    
+    nfcDb = NfcDb(dbPath)
+
+    nfc = nfcDb.get(0)
+    assert nfc == None
+
+def test_db_nfc_getAll():
+    dbPath = 'test_db_nfc_getAll.db'
+
+    if os.path.isfile(dbPath):
+        os.remove(dbPath)
+    
+    nfcDb = NfcDb(dbPath)
+
+    nfcDb.add('', 0)
+    nfcDb.add('', 0)
+
+    nfcList = nfcDb.getAll()
+    assert len(nfcList) == 2
+
+def test_db_nfc_set():
+    dbPath = 'test_db_nfc_set.db'
+
+    if os.path.isfile(dbPath):
+        os.remove(dbPath)
+    
+    nfcDb = NfcDb(dbPath)
+
+    nfc1 = nfcDb.add('012345', 0)
+    if nfc1 == None:
+        assert False
+
+    nfc2 = nfcDb.set( nfc1['id'], 'abcdef', 1 )
+    if nfc2 == None:
+        assert False
+    
+    assert nfc1['id'] == nfc2['id']
+    assert nfc2['nfcId'] == 'abcdef'
+    assert nfc2['userId'] == 1
+
+def test_db_nfc_set_not_exist():
+    dbPath = 'test_db_nfc_set_not_exist.db'
+
+    if os.path.isfile(dbPath):
+        os.remove(dbPath)
+    
+    nfcDb = NfcDb(dbPath)
+
+    nfc = nfcDb.set(0, 'abcdef', 1)
+    assert nfc == None
+
+def test_db_nfc_delete():
+    dbPath = 'test_db_nfc_delete.db'
+
+    if os.path.isfile(dbPath):
+        os.remove(dbPath)
+    
+    nfcDb = NfcDb(dbPath)
+
+    nfc1 = nfcDb.add('', 0)
+    if nfc1 == None:
+        assert False
+
+    nfcDb.delete(nfc1['id'])
+
+    nfc2 = nfcDb.get(nfc1['id'])
+    assert nfc2 == None
+
+def test_db_nfc_delete_not_exist():
+    dbPath = 'test_db_nfc_delete_not_exist.db'
+
+    if os.path.isfile(dbPath):
+        os.remove(dbPath)
+    
+    nfcDb = NfcDb(dbPath)
+
+    nfcDb.delete(0)
+
+    nfc = nfcDb.get(0)
+    assert nfc == None
