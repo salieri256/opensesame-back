55ce2afa9cbb2967c798d5f0e02782a92e01bf46
さりえり <nakakou201250@gmail.com>
2022-08-15 02:48:31 +0900

    Implement servo motor control

--- src/libs/door.py
+++ src/libs/door.py
@@ -1,8 +1,25 @@
+from gpiozero.pins.native import NativeFactory
+from gpiozero import Servo
+
+factory = NativeFactory()
+pig = Servo(12, min_pulse_width=0.5/1000, max_pulse_width=2.4/1000, pin_factory=factory)
+
+def rotate_m90():
+    pig.min()
+
+def rotate_0():
+    pig.mid()
+
+def rotate_90():
+    pig.max()
+
 class Door:
     def lock(self):
+        rotate_m90()
         print('lock door')
 
     def unlock(self):
+        rotate_90()
         print('unlock door')
 
 door = Door()
--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from src.routers import user, log
+from src.routers import user, log, door
 
 app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(log.router)
+app.include_router(door.router)
