030e5977a9ea25940b70a5f5d1ff010cf5a5d9f1
さりえり <nakakou201250@gmail.com>
2022-08-18 15:45:04 +0900

    Read NFC ID

--- src/libs/read_nfc_id.py
+++ src/libs/read_nfc_id.py
@@ -1,6 +1,15 @@
 import binascii
+import nfc
 
 def onConnected(tag):
     idm = binascii.hexlify(tag.identifier)
     id_str = idm.decode()
     print(id_str)
+
+def main():
+    clf = nfc.ContactlessFrontend('usb')
+    clf.connect(rdwr={'on-connected': onConnected})
+    clf.close()
+
+if __name__ == '__main__':
+    main()
--- src/main.py
+++ src/main.py
@@ -1,14 +1,8 @@
 from fastapi import FastAPI
 from src.routers import user, log, door
-import nfc
-from src.libs.read_nfc_id import onConnected
 
 app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(log.router)
 app.include_router(door.router)
-
-clf = nfc.ContactlessFrontend('usb')
-clf.connect(rdwr={'on-connected': onConnected})
-clf.close()
