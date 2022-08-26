5e1de59e21a622b6cf535ba68ada7560216e5bc0
さりえり <nakakou201250@gmail.com>
2022-08-26 13:11:49 +0900

    Allow CORS

--- db_service/src/main.py
+++ db_service/src/main.py
@@ -1,5 +1,13 @@
+import os
+import json
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.routers import user, active, door, lock, activity_log, lock_log
+
+ALLOW_ORIGINS = json.loads( os.environ['ALLOW_ORIGINS'] )
+ALLOW_METHODS = json.loads( os.environ['ALLOW_METHODS'] )
+ALLOW_HEADERS = json.loads( os.environ['ALLOW_HEADERS'] )
+ALLOW_CREDENTIALS = True if os.environ['ALLOW_CREDENTIALS'] == 'True' else False
 
 app = FastAPI()
 
@@ -9,3 +17,11 @@ app.include_router(door.router)
 app.include_router(lock.router)
 app.include_router(activity_log.router)
 app.include_router(lock_log.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOW_ORIGINS,
+    allow_credentials=ALLOW_CREDENTIALS,
+    allow_methods=ALLOW_METHODS,
+    allow_headers=ALLOW_HEADERS,
+)
--- door_lock/src/main.py
+++ door_lock/src/main.py
@@ -1,5 +1,13 @@
+import os
+import json
 from fastapi import FastAPI, Depends, Response
+from fastapi.middleware.cors import CORSMiddleware
 from src.door import Door, get_door
+
+ALLOW_ORIGINS = json.loads( os.environ['ALLOW_ORIGINS'] )
+ALLOW_METHODS = json.loads( os.environ['ALLOW_METHODS'] )
+ALLOW_HEADERS = json.loads( os.environ['ALLOW_HEADERS'] )
+ALLOW_CREDENTIALS = True if os.environ['ALLOW_CREDENTIALS'] == 'True' else False
 
 app = FastAPI()
 
@@ -12,3 +20,11 @@ def lock_door(door: Door = Depends(get_door)):
 def unlock_door(door: Door = Depends(get_door)):
     door.unlock()
     return Response()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOW_ORIGINS,
+    allow_credentials=ALLOW_CREDENTIALS,
+    allow_methods=ALLOW_METHODS,
+    allow_headers=ALLOW_HEADERS,
+)
