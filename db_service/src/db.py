8f6419470cbd9c746f42643d4538f5702769a7c7
さりえり <nakakou201250@gmail.com>
2022-08-26 23:29:02 +0900

    Generate a session for each access

--- db_service/src/db.py
+++ db_service/src/db.py
@@ -11,14 +11,12 @@ DB_NAME = os.environ['DB_NAME']
 ASYNC_DB_URL = '{}://{}:{}@{}:{}/{}'.format('postgresql+asyncpg', DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
 
 async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
-async_session = scoped_session(
-    sessionmaker(
-        autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession
-    )
+async_session = sessionmaker(
+    autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession
 )
 
 Base = declarative_base()
 
 async def get_db():
-    async with async_session() as session:
+    async with scoped_session( async_session )() as session:
         yield session
